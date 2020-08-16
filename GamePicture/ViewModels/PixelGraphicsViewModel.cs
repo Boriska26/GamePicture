d98308da236692418e267a6a8fab065b81e58720
dytell <vbv.5842@gmail.com>
2020-08-16 21:45:07 +0700

    Разработка_Интерфейса

---
+++ GamePicture/ViewModels/PixelGraphicsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePicture.ViewModels
+{
+    public class PixelGraphicsViewModel : BaseViewModel
+    {
+
+    }
+}
