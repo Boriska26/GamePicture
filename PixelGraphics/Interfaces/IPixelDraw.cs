abe5228d897d2f9b2ce447f337ae2653fb44a786
dytell <vbv.5842@gmail.com>
2020-08-16 20:48:36 +0700

    Добавьте файлы проекта.

---
+++ PixelGraphics/Interfaces/IPixelDraw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelGraphics.Interfaces
+{
+    interface IPixelDraw
+    {
+        public void Draw();
+    }
+}
